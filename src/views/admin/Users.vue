fb87bc38e423aaf81f34f6070c677fc5aedd9ba6
Ilja Busch <ilja.ragnar@gmail.com>
2021-12-16 16:03:50 +0100

    fixed typo

--- src/views/admin/Users.vue
+++ src/views/admin/Users.vue
@@ -117,7 +117,11 @@ export default class Users extends Vue {
 
   async mounted (): Promise<void> {
     const service = feathersClient.service('users');
-    const res = await service.find({ $limit: 10000 });
+    const res = await service.find({
+      query: {
+        $paginate: false,
+      },
+    });
 
     this.permissions = Object.values(UserPermissions) as unknown as string[];
     console.log(Object.values(UserPermissions));
