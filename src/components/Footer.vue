019ed347c3a2e078c099f810fd91c364cd04a7e2
Ilja Busch <ilja.ragnar@gmail.com>
2022-03-03 20:28:01 +0100

    New & better looking footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,23 +1,11 @@
 <template>
-  <div class="p-4 bg-gray-800 text-gray-100">
-    <!--    <div class="flex justify-between whitespace-nowrap flex-wrap text-sm space-y-2 text-gray-600">-->
-    <!--      <router-link-->
-    <!--        v-for="(route, i) in $router.options.routes.filter(r => !(exclude.includes(r.path) || (r.path && r.path.includes('admin')) || r.path.includes('businesses')))"-->
-    <!--        :key="i" class="mx-2 my-2 hover:text-gray-300 transition-colors cursor-pointer"-->
-    <!--        :to="route.path">-->
-    <!--        <span v-if="route.name === 'home'">-->
-    <!--          Home-->
-    <!--        </span>-->
-    <!--        <span v-else>-->
-    <!--          {{ route.path.split(':')[0] }}-->
-    <!--        </span>-->
-    <!--      </router-link>-->
-    <!--    </div>-->
+  <div class="p-4 bg-slate-800 dark:bg-black text-gray-100">
+    <div class="flex flex-row justify-between text-gray-400">
+      <div class="flex flex-col">
+        <div>© RedwoodRP 2021-{{ year }} — All rights reserved</div>
+        <div>Made by JustMe#8491 with ❤</div>
+      </div>
 
-    <!-- Copyright -->
-    <div class="flex flex-col items-center text-gray-400 mt-2">
-      <div>© RedwoodRP 2021-{{ year }} — All rights reserved</div>
-      <div>Made by JustMe#8491 with ❤</div>
       <div id="DMCA-badge" class="mt-3 rounded">
         <div class="dm-1 dm-1-b flex justify-center items-center"
              style="left: 0; background-color: rgb(28, 106, 153); border: none !important;">
